0bae0b8086d0155bc26180150bbcc8565fb28841
Patrick <bakospatrick@gmail.com>
2019-04-15 21:20:47 +0200

    colored, no visible space between characters

--- src/com/codecool/termlib/GameBoard.java
+++ src/com/codecool/termlib/GameBoard.java
@@ -4,23 +4,23 @@ class GameBoard {
     private static int WIDTH = 20;
     private static int HEIGHT = 20;
     private static String[][] grid =  new String[HEIGHT][WIDTH];
-    private static String fillChar = Character.toString(0x2586);
+    private static String fillChar = Character.toString('\u2588');
     public void init() {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid.length; j++) {
                 if (i == 0 && j > 0 && j < grid.length - 1) {
-                    grid[i][j] = "\u001B[31m" + fillChar;
+                    grid[i][j] = "\u001B[31;41m" + fillChar;
                 }
                 else if (i == grid.length - 1 && j > 0 && j < grid.length - 1) {
-                    grid[i][j] = "\u001B[34m" + fillChar;
+                    grid[i][j] = "\u001B[34;44m" + fillChar;
                 }
                 else if (j == 0 && i > 0 && i < grid.length - 1) {
-                    grid[i][j] = "\u001B[33m" + fillChar;
+                    grid[i][j] = "\u001B[33;43m" + fillChar;
                 }
                 else if (j == grid.length - 1 && i > 0 && i < grid.length - 1) {
-                    grid[i][j] = "\u001B[32;m" + fillChar;
+                    grid[i][j] = "\u001B[32;42m" + fillChar;
                 }
-                else { grid[i][j] = "\u001B[30m" + fillChar; }
+                else { grid[i][j] = "\u001B[30;40m" + fillChar; }
             }
         }
         for (String[] i:grid) {
@@ -28,7 +28,7 @@ class GameBoard {
                 System.out.print(j);
                 System.out.print(' ');
             }
-            System.out.print('\n');
+            System.out.print("\u001B[0m" + "\n"); // need to reset background for last chars in line
         }
     }
 }
