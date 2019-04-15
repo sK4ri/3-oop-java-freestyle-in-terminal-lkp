2f00ae7756a92893760f8d99674f6237fcefc97a
Patrick <bakospatrick@gmail.com>
2019-04-15 20:03:51 +0200

    basic grid

---
+++ src/com/codecool/termlib/Game.java
@@ -0,0 +1,9 @@
+package com.codecool.termlib;
+
+public class Game {
+    private static GameBoard start = new GameBoard();
+
+    public static void main(String[] args) {
+        start.init();
+    }
+}
--- src/com/codecool/termlib/GameBoard.java
+++ src/com/codecool/termlib/GameBoard.java
@@ -1,18 +1,34 @@
 package com.codecool.termlib;
 
-class GameBorad {
-    private static int WIDTH = 50;
-    private static int HEIGHT = 50;
-    private static String[][] grid = String[HEIGHT][WIDTH];
+class GameBoard {
+    private static int WIDTH = 20;
+    private static int HEIGHT = 20;
+    private static String[][] grid =  new String[HEIGHT][WIDTH];
+    private static String fillChar = Character.toString(0x2586);
     public void init() {
-        for (int i=0;i<grid.length;i++) {
-            for (int j=0;grid[j].length;j++) {
-                if (i==0 || i==grid.length || j==0 || j==grid.length) {
-                    grid[i][j] = Character.toString(0x2586);
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                if (i == 0 && j > 0 && j < grid.length - 1) {
+                    grid[i][j] = "\u001B[31m" + fillChar;
                 }
+                else if (i == grid.length - 1 && j > 0 && j < grid.length - 1) {
+                    grid[i][j] = "\u001B[34m" + fillChar;
+                }
+                else if (j == 0 && i > 0 && i < grid.length - 1) {
+                    grid[i][j] = "\u001B[33m" + fillChar;
+                }
+                else if (j == grid.length - 1 && i > 0 && i < grid.length - 1) {
+                    grid[i][j] = "\u001B[32;m" + fillChar;
+                }
+                else { grid[i][j] = "\u001B[30m" + fillChar; }
             }
         }
-    public static void main(String[] args) {
-        init();
+        for (String[] i:grid) {
+            for (String j: i) {
+                System.out.print(j);
+                System.out.print(' ');
+            }
+            System.out.print('\n');
+        }
     }
 }
