92a37051cf9d24da6d120421b118457c92246fcc
Patrick <bakospatrick@gmail.com>
2019-04-17 14:14:53 +0200

    mittomen

--- src/com/codecool/termlib/Game.java
+++ src/com/codecool/termlib/Game.java
@@ -2,12 +2,17 @@ package com.codecool.termlib;
 
 public class Game {
     public static void main(String[] args) {
+        // Timer initialization
         int gameTime = 6;
         new Countdown(gameTime);
         System.out.println("Start Game!");
+
+        // Game board initialization
         GameBoard myBoard = new GameBoard();
         CreateColoredBlock colorBlock = new CreateColoredBlock();
         String[][] myGrid = myBoard.init();
+
+        //Game board printing
         colorBlock.placeRandomColoredBlock(myGrid);
         myBoard.printBoard(myGrid);
     }
