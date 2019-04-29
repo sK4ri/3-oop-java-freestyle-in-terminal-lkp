51a44b7f20c2f0e0022210e037e6f73a59d11d46
Patrick <bakospatrick@gmail.com>
2019-04-29 09:46:21 +0200

    last version

--- src/com/codecool/termlib/Game.java
+++ src/com/codecool/termlib/Game.java
@@ -1,15 +1,15 @@
 package com.codecool.termlib;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Game {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Timer initialization
         int gameTime = 20;
         new Countdown(gameTime);
         System.out.println("Start Game!");
         Scanner myObj = new Scanner(System.in);
-        String characterPressed = myObj.nextLine();
 
         // Game board initialization
         GameBoard myBoard = new GameBoard();
@@ -20,5 +20,9 @@ public class Game {
         colorBlock.placeRandomColoredBlock(myGrid);
         myBoard.printBoard(myGrid);
 
+        Input terminalSetup = new Input();
+        terminalSetup.raw();
+        String characterPressed = myObj.nextLine();
+        System.out.println(characterPressed);
     }
 }
