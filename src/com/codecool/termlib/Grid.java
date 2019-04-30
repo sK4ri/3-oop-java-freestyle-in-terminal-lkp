c78ce3afe2b89c3231f1fd93da54eccb8746a607
lupakd <daniel.lupis@gmail.com>
2019-04-30 16:04:14 +0200

    Array created, table is printable. There is a minor problem with indexing (outofbounds).

--- src/com/codecool/termlib/Grid.java
+++ src/com/codecool/termlib/Grid.java
@@ -1,8 +1,6 @@
 package com.codecool.termlib;
 
-import java.lang.*;
 import java.util.Random;
-
 
 class Grid {
 
@@ -15,29 +13,41 @@ class Grid {
 
     static char[] charList = {UPLEFT, UPRIGHT, DOWNLEFT, DOWNRIGHT, HORIZONTAL, VERTICAL};
 
+    static String[] alph = {"A", "B", "C", "D"};
+
+    static String[][] table = new String[8][8];
+
     static Random randNum = new Random();
 
-    static void mainGrid () {
-        for (int i = 0; i <= 6; i++) {
-            for (int j = 0; j <= 6; j++) {
-                if (i == 0 && j == 0) {
-                    System.out.print(UPLEFT);
-                } else if (i == 0 && j == 6) {
-                    System.out.print(UPRIGHT);
-                } else if (j == 0 && i == 6) {
-                    System.out.print(DOWNLEFT);
+    static String[][] mainGrid () {
+        for (int i = 0; i <= 7; i++) {
+            for (int j = 0; j <= 7; j++) {
+                if (i == 0 && j > 1 && j < 6) {
+                    table[i][j] = Integer.toString(j-1);
+                } else if (i == 0 && j < 1 || j > 6) {
+                    table[i][j] = " ";
+                } else if (j == 0 && i > 1 && i < 6) {
+                    table[i][j] = alph[i-2];
+                } else if (j == 0 || i < 1 || i > 6) {
+                    table[i][j] = " ";
+                } else if (i == 1 && j == 1) {
+                    table[i][j] = Character.toString(UPLEFT);
+                } else if (i == 1 && j == 6) {
+                    table[i][j] = Character.toString(UPRIGHT);
+                } else if (j == 1 && i == 6) {
+                    table[i+1][j] = Character.toString(DOWNLEFT);
                 } else if (j == 6 && i == 6) {
-                    System.out.print(DOWNRIGHT);
-                } else if (j == 6 || j == 0) {
-                    System.out.print(VERTICAL);
-                } else if (i == 6 || i == 0){
-                    System.out.print(HORIZONTAL);
+                    table[i][j] = Character.toString(DOWNRIGHT);
+                } else if (j == 6 || j == 1) {
+                    table[i][j] = Character.toString(VERTICAL);
+                } else if (i == 6 || i == 1) {
+                    table[i][j] = Character.toString(HORIZONTAL);
                 } else {
-                    System.out.print(charList[randNum.nextInt(charList.length)]);
+                    table[i][j] = Character.toString(charList[randNum.nextInt(charList.length)]);
                 }
             }
-            System.out.println();
         }
+    return table;
     }
 }
 
--- src/com/codecool/termlib/Main.java
+++ src/com/codecool/termlib/Main.java
@@ -2,6 +2,11 @@ package com.codecool.termlib;
 
 public class Main {
     public static void main (String[] args) {
-        Grid.mainGrid();
+        for (int i = 0; i < Grid.mainGrid().length; i++) {
+            for (int j = 0; j < Grid.mainGrid()[i].length; j++){
+                System.out.print(Grid.mainGrid()[i][j]);
+            }
+        System.out.println();
+        }
     }
 }
