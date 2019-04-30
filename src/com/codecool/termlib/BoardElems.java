a05d7ed27333e3a9d812f9c413b791d90d017f11
sK4ri <skari.simon@gmail.com>
2019-04-30 10:58:30 +0200

    ASCII Board elements

---
+++ src/com/codecool/termlib/BoardElems.java
@@ -0,0 +1,10 @@
+package com.codecool.termlib;
+
+public class BoardElems {
+    char UPRIGHT = (char)9491;
+    char UPLEFT = (char)9487;
+    char DOWNRIGHT = (char)9499;
+    char DOWNLEFT = (char)9495;
+    char HORIZONTAL = (char)9473;
+    char VERTICAL = (char)9475;
+}
---
+++ src/com/codecool/termlib/Game.java
@@ -0,0 +1,31 @@
+package com.codecool.termlib;
+
+public class Game {
+    public static void main(String[] args) {
+        System.out.println("UL: "+(char)9487);
+        System.out.println("UR: "+(char)9491);
+        System.out.println("DL: "+(char)9495);
+        System.out.println("DR: "+(char)9499);
+        System.out.println("H: "+(char)9473);
+        System.out.println("V: "+(char)9475);
+        System.out.println(" ");
+        System.out.print((char)9487);
+        System.out.print((char)9473);
+        System.out.print((char)9473);
+        System.out.print((char)9473);
+        System.out.print((char)9491+"\n");
+        System.out.print((char)9475);
+        System.out.print("X");
+        System.out.print("X");
+        System.out.print("X");
+        System.out.print((char)9475+"\n");
+        System.out.print((char)9495);
+        System.out.print((char)9473);
+        System.out.print((char)9473);
+        System.out.print((char)9473);
+        System.out.print((char)9499);
+
+
+
+    }
+}
